e5ba485dee18d40a9dd0d5b19b7b59191b2e9b55
DanilaVV <voznyuk.danila@yandex.ru>
2025-03-21 07:07:43 +0000

    commit2

--- ex08/ft_ten_queens_puzzle.c
+++ ex08/ft_ten_queens_puzzle.c
@@ -39,33 +39,6 @@ void	ft_show(int ms[10][10])
 	write(1, "\n", 1);
 }
 
-int	ft_check_diag(int ms[10][10], int i_cur, int j_cur)
-{
-	int	i;
-	int	j;
-	i = i_cur;
-	j = j_cur;
-	while (i < 10 && j < 10)
-		if (ms[i++][j++] == 2)
-			return (0);
-	i = i_cur;
-	j = j_cur;
-	while (i >= 0 && j >= 0)
-		if (ms[i--][j--] == 2)
-			return (0);
-	i = i_cur;
-	j = j_cur;
-	while (j >= 0 && i < 10)
-		if (ms[i++][j--] == 2)
-			return (0);
-	i = i_cur;
-	j = j_cur;
-	while (j < 10 && i >= 0)
-		if (ms[i--][j++] == 2)
-			return (0);
-	return (1);
-}
-
 int	ft_check(int ms[10][10], int i_cur, int j_cur)
 {
 	int	i;
@@ -74,13 +47,20 @@ int	ft_check(int ms[10][10], int i_cur, int j_cur)
 	i = 0;
 	j = 0;
 	while (j < 10)
-		if (ms[i_cur][j++] == 2)
-			return (0);
-	while (i < 10)
-		if (ms[i++][j_cur] == 2)
-			return (0);
-	if (!ft_check_diag(ms, i_cur, j_cur))
-		return (0);
+	{
+		while (i < 10)
+		{
+			if ((i == i_cur || j == j_cur) && ms[i][j] == 2)
+				return (0);
+			if ((i - j == i_cur - j_cur) && ms[i][j] == 2)
+				return (0);
+			if ((i + j == i_cur + j_cur) && ms[i][j] == 2)
+				return (0);
+			i++;
+		}
+		i = 0;
+		j++;
+	}
 	return (1);
 }
 
@@ -93,19 +73,19 @@ void	ft_fill(int ms[10][10], int i_cur, int j_cur)
 	j = 0;
 	if (!ft_check(ms, i_cur, j_cur))
 		return ;
-	ms[i_cur][j_cur] = 2;
 	while (j < 10)
-		ms[i_cur][j++] = 1;
-	while (i < 10)
-		ms[i++][j_cur] = 1;
-	i = i_cur;
-	j = j_cur;
-	while (i < 10 && j < 10)
-		ms[i++][j++] = 1;
-	i = i_cur;
-	j = j_cur;
-	while (i >= 0 && j >= 0)
-		ms[i--][j--] = 1;
+	{
+		while (i < 10)
+		{
+			if (i == i_cur || j == j_cur)
+				ms[i][j] = 1;
+			if (i - j == i_cur - j_cur || i + j == i_cur + j_cur)
+				ms[i][j] = 1;
+			i++;
+		}
+		i = 0;
+		j++;
+	}
 	ms[i_cur][j_cur] = 2;
 }
 
@@ -134,7 +114,16 @@ void	show(int ms[10][10])
 
 int	ft_ten_queens_puzzle(void)
 {
-	
+	int	ms[10][10];
+	int	ch;
+	int	i;
+	int	j;
+
+	ch = 0;
+	i = 0;
+	j = 0;
+	while
+
 }
 int	main(void)
 {
@@ -148,7 +137,7 @@ int	main(void)
 	show(ms);
 	ft_fill(ms, 3, 7);
 	show(ms);
-	ft_fill(ms, 4, 5);
+	ft_fill(ms, 4, 6);
 	show(ms);
 	return (0);
 }
