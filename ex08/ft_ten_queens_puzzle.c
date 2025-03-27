d3d7f6770e97f2116ac6cbcd2e92491d453337be
DanilaVV <voznyuk.danila@yandex.ru>
2025-03-27 05:40:33 +0000

    commit3

--- ex08/ft_ten_queens_puzzle.c
+++ ex08/ft_ten_queens_puzzle.c
@@ -1,6 +1,33 @@
 #include <unistd.h>
+#include <stdio.h>
 
-void	ft_clear(int ms[10][10])
+int	g_ms[10][10];
+int	g_ims[10];
+int	g_res_ch;
+/*void    show(void)
+{
+        int             i;
+        int             j;
+        char    c;
+
+        i = 0;
+        j = 0;
+        while (i < 10)
+        {
+                while (j < 10)
+                {
+                        c = '0' + g_ms[i][j];
+                        write(1, &c, 1);
+                        j++;
+                }
+                j = 0;
+                i++;
+                write(1, "\n", 1);
+        }
+        write(1, "\n", 1);
+}*/
+
+void	ft_clear(void)
 {
 	int	i;
 	int	j;
@@ -10,36 +37,39 @@ void	ft_clear(int ms[10][10])
 	while (i < 10)
 	{
 		while (j < 10)
-			ms[i][j++] = 0;
+			g_ms[i][j++] = 0;
+		g_ims[i] = 0;
 		i++;
 		j = 0;
 	}
 }
 
-void	ft_show(int ms[10][10])
+void	ft_show(void)
 {
 	int		i;
 	int		j;
 	char	c;
 
-	while (i < 10)
+	i = 0;
+	j = 0;
+	while (j < 10)
 	{
-		while (j < 10)
+		while (i < 10)
 		{
-			if (ms[i][j] == 1)
+			if (g_ms[i][j] == 2)
 			{
 				c = i + '0';
 				write(1, &c, 1);
 			}
-			j++;
+			i++;
 		}
-		i++;
-		j = 0;
+		j++;
+		i = 0;
 	}
 	write(1, "\n", 1);
 }
 
-int	ft_check(int ms[10][10], int i_cur, int j_cur)
+int	ft_check(int i_cur, int j_cur)
 {
 	int	i;
 	int	j;
@@ -50,11 +80,11 @@ int	ft_check(int ms[10][10], int i_cur, int j_cur)
 	{
 		while (i < 10)
 		{
-			if ((i == i_cur || j == j_cur) && ms[i][j] == 2)
+			if ((i == i_cur || j == j_cur) && g_ms[i][j] == 2)
 				return (0);
-			if ((i - j == i_cur - j_cur) && ms[i][j] == 2)
+			if ((i - j == i_cur - j_cur) && g_ms[i][j] == 2)
 				return (0);
-			if ((i + j == i_cur + j_cur) && ms[i][j] == 2)
+			if ((i + j == i_cur + j_cur) && g_ms[i][j] == 2)
 				return (0);
 			i++;
 		}
@@ -64,80 +94,58 @@ int	ft_check(int ms[10][10], int i_cur, int j_cur)
 	return (1);
 }
 
-void	ft_fill(int ms[10][10], int i_cur, int j_cur)
+void	ft_fill(int i_cur, int j_cur)
 {
-	int	i;
-	int	j;
-
-	i = 0;
-	j = 0;
-	if (!ft_check(ms, i_cur, j_cur))
+	if (i_cur == 0 && j_cur == 0)
+		ft_clear();
+	if (j_cur == 10)
 		return ;
-	while (j < 10)
+	if (i_cur == 10)
 	{
-		while (i < 10)
-		{
-			if (i == i_cur || j == j_cur)
-				ms[i][j] = 1;
-			if (i - j == i_cur - j_cur || i + j == i_cur + j_cur)
-				ms[i][j] = 1;
-			i++;
-		}
-		i = 0;
-		j++;
+		g_ms[g_ims[j_cur - 1]][j_cur - 1] = 0;
+		ft_fill(g_ims[j_cur - 1] + 1, j_cur - 1);
+		return ;
 	}
-	ms[i_cur][j_cur] = 2;
-}
-
-void	show(int ms[10][10])
-{
-	int		i;
-	int		j;
-	char	c;
-
-	i = 0;
-	j = 0;
-	while (i < 10)
+	if (!ft_check(i_cur, j_cur))
 	{
-		while (j < 10)
-		{
-			c = '0' + ms[i][j];
-			write(1, &c, 1);
-			j++;
-		}
-		j = 0;
-		i++;
-		write(1, "\n", 1);
+		ft_fill(i_cur + 1, j_cur);
+		return ;
 	}
-	write(1, "\n", 1);
+	g_ims[j_cur] = i_cur;
+	g_ms[i_cur][j_cur] = 2;
+	ft_fill(0, j_cur + 1);
 }
 
 int	ft_ten_queens_puzzle(void)
 {
-	int	ms[10][10];
-	int	ch;
 	int	i;
 	int	j;
 
-	ch = 0;
-	i = 0;
-	j = 0;
-	while
-
+	g_res_ch = 1;
+	ft_fill(0, 0);
+	ft_show();
+	j = 9;
+	while (g_ims[0] != 9 || g_ims[1] != 7 || g_ims[9] != 3)
+	{
+		i = g_ims[j];
+		while (i < 9)
+		{
+			g_ms[i++][j] = 0;
+			if (ft_check(i, j))
+			{
+				ft_fill(i, j);
+				j = 9;
+				i = g_ims[j];
+				ft_show();
+				g_res_ch++;
+			}
+		}
+		g_ms[i][j--] = 0;
+	}
+	return (g_res_ch);
 }
 int	main(void)
 {
-	int	i;
-	int	j;
-	int	ms[10][10];
-
-	i = 0;
-	j = 0;
-	ft_clear(ms);
-	show(ms);
-	ft_fill(ms, 3, 7);
-	show(ms);
-	ft_fill(ms, 4, 6);
-	show(ms);
+	printf("%d\n", ft_ten_queens_puzzle());
 	return (0);
 }
